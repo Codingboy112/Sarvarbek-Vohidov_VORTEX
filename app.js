c90a9d356694d5b6a4a00cc67a963f5becf3534f
tyler <tylertheunknownss@gmail.com>
2025-04-05 16:17:28 +0500

    Header and hero done

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let navbarBtn = document.querySelector('.navbar__icon');
+let dropdownLinks = document.querySelector('.links__dropdown');
+
+navbarBtn.addEventListener("click", () => {
+  dropdownLinks.classList.toggle("links__dropdown--active");
+});
+
+
 
 
 window.addEventListener("load", function() {
