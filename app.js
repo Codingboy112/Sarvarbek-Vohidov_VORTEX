801d0b9187c764941a0243f1edc3b071cfd662ff
tyler <tylertheunknownss@gmail.com>
2025-04-06 16:10:18 +0500

    Basic things done (Essential Informations and image remainign...)

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-let navbarBtn = document.querySelector('.navbar__icon');
-let dropdownLinks = document.querySelector('.links__dropdown');
+let navbarBtn = document.querySelector(".navbar__icon");
+let dropdownLinks = document.querySelector(".links__dropdown");
+let elForm = document.querySelector("#user__details");
+let elName = document.querySelector("#userName");
+let elEmail = document.querySelector("#userEmail");
+let elNumber = document.querySelector("#userPhone");
+let userMessage = document.querySelector("#userMessage");
 
 navbarBtn.addEventListener("click", () => {
   dropdownLinks.classList.toggle("links__dropdown--active");
 });
 
+elForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const token = "8167034934:AAGdbvHormNNr1r-AMgKMZm6-gVhkI9eVpY";
+  const chatId = "6959013020";
 
+  let telegramSend = `New People 🌎
+  User Name: ${elName.value}
+  User Email: ${elEmail.value}
+  User Phone Number: ${elNumber.value}
+  User Message: ${userMessage.value}`;
 
+  const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${telegramSend}`;
+
+  fetch(url, { method: "GET" })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.ok) {
+        console.log("Message sent successfully!");
+      } else {
+        console.error("Error sending message:", data);
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+
+  elName.value = "";
+  elEmail.value = "";
+  elNumber.value = "";
+  userMessage.value = "";
+});
