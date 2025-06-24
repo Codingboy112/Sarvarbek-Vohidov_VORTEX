c5c4bdff9bd1fcf0f5418de36dbc563e62758991
tyler <tylertheunknownss@gmail.com>
2025-06-24 16:35:50 +0500

    New things such as user track and certificate show has been added

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ let elName = document.querySelector("#userName");
 let elEmail = document.querySelector("#userEmail");
 let elNumber = document.querySelector("#userPhone");
 let userMessage = document.querySelector("#userMessage");
-let toTop = document.querySelector('.toTop');
+let toTop = document.querySelector(".toTop");
+let isShowedImage = true;
 
-
+let dropdownCertificateEng = document.querySelector(".dropdownCertificateEng");
+let dropdownCertificateFront1 = document.querySelector(
+  ".dropdownCertificateFront1"
+);
+let dropdownCertificateFront2 = document.querySelector(
+  ".dropdownCertificateFront2"
+);
 
 navbarBtn.addEventListener("click", () => {
   dropdownLinks.classList.toggle("links__dropdown--active");
@@ -40,32 +47,77 @@ elForm.addEventListener("submit", (event) => {
   userMessage.value = "";
 });
 
-
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
   const currentScrollY = window.scrollY;
   if (currentScrollY < lastScrollY) {
-    toTop.style.bottom = "50px"
+    toTop.style.bottom = "50px";
   } else if (currentScrollY > lastScrollY) {
-    toTop.style.bottom = "-100px"
+    toTop.style.bottom = "-100px";
   }
 
   if (currentScrollY === 0) {
-    toTop.style.bottom = "-100px"
+    toTop.style.bottom = "-100px";
   }
- 
+
   lastScrollY = currentScrollY;
 });
 
-
-toTop.addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+toTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 
   const scrollCheck = setInterval(() => {
     if (window.scrollY === 0) {
       toTop.style.bottom = "-100px";
       clearInterval(scrollCheck);
     }
-  }, 50); 
+  }, 50);
 });
+
+let certificateDropdown = document.querySelector("#certificateDropdown");
+let singleCloseBtn = document.querySelector("#singleCloseBtn");
+let certificateImage = document.querySelector("#certificateImage");
+let coverTheArea = document.querySelector(".coverTheArea");
+
+function doCertificateDrop(imgPath) {
+  if (!isShowedImage) {
+    coverTheArea.style.display = "inline-block";
+    certificateDropdown.style.display = "inline-block";
+    certificateImage.setAttribute('src', imgPath);
+    isShowedImage = true;
+    document.body.style.overflow = 'hidden';
+document.documentElement.style.overflow = 'hidden';
+  } else{
+      coverTheArea.style.display = "none";
+    certificateDropdown.style.display = "none";
+    certificateImage.setAttribute('src', '');
+       isShowedImage = false;
+       document.body.style.overflowY = 'scroll';
+document.documentElement.style.overflowY = 'scroll';
+  }
+}
+
+doCertificateDrop("./assets/image.png");
+
+
+dropdownCertificateEng.addEventListener('click', ()=>{
+  isShowedImage = false;
+  doCertificateDrop("./assets/image.png");
+})
+
+dropdownCertificateFront1.addEventListener('click', ()=>{
+  isShowedImage = false;
+  doCertificateDrop("./assets/image2.png");
+})
+
+dropdownCertificateFront2.addEventListener('click', ()=>{
+  isShowedImage = false;
+  doCertificateDrop("./assets/image3.png");
+})
+
+window.addEventListener('click', (e)=>{
+ if (e.target.closest('#singleCloseBtn') || e.target.closest('.coverTheArea')) {
+  doCertificateDrop('')
+ }
+})
